0db1e5f7b51fb87e91aeadc3f3fb5eefd83852dc
Justin Fuller <justindanielfuller@gmail.com>
2023-02-17 15:15:51 -0500

    GoString

--- multierror.go
+++ multierror.go
@@ -3,10 +3,29 @@ package multierror
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 type multiError struct {
 	errors []error
+}
+
+func (m *multiError) GoString() string {
+	if m == nil {
+		return "[]error{nil}"
+	}
+
+	var errs []error
+	for _, e := range m.errors {
+		errs = append(errs, flatten(e)...)
+	}
+
+	var errStrings []string
+	for _, err := range errs {
+		errStrings = append(errStrings, err.Error())
+	}
+
+	return fmt.Sprintf("[%d]error{%s}", len(errStrings), strings.Join(errStrings, ","))
 }
 
 func (m *multiError) String() string {
--- multierror_test.go
+++ multierror_test.go
@@ -49,6 +49,14 @@ func TestMultiError(t *testing.T) {
 		t.Errorf("Unexpected string, got %s", s)
 	}
 
+	if s := multierror.Append(errSentinelOne, errSentinelTwo, errSentinelThree).String(); s != "Found 3 errors:\n\tsentinel one\n\tsentinel two\n\tsentinel three\n" {
+		t.Errorf("Unexpected string, got %s", s)
+	}
+
+	if s := multierror.Append(errSentinelOne, errSentinelTwo, errSentinelThree).GoString(); s != "[3]error{sentinel one,sentinel two,sentinel three}" {
+		t.Errorf("Unexpected string, got %s", s)
+	}
+
 	err1 := multierror.Append(errSentinelOne, errSentinelTwo)
 	err2 := multierror.Append(errSentinelThree, errSentinelFour)
 	if err := multierror.Append(err1, err2); !errors.Is(err, errSentinelOne) || !errors.Is(err, errSentinelTwo) || !errors.Is(err, errSentinelThree) || !errors.Is(err, errSentinelFour) {
@@ -57,6 +65,14 @@ func TestMultiError(t *testing.T) {
 
 	if s := multierror.Append(err1, err2).Error(); s != "Found 4 errors:\n\tsentinel one\n\tsentinel two\n\tsentinel three\n\tsentinel four\n" {
 		t.Errorf("(Recursive) Unexpected string, got %s", s)
+	}
+
+	if s := multierror.Append(err1, err2).String(); s != "Found 4 errors:\n\tsentinel one\n\tsentinel two\n\tsentinel three\n\tsentinel four\n" {
+		t.Errorf("(Recursive) Unexpected string, got %s", s)
+	}
+
+	if s := multierror.Append(err1, err2).GoString(); s != "[4]error{sentinel one,sentinel two,sentinel three,sentinel four}" {
+		t.Errorf("(Recursive) Unexpected GoString, got %s", s)
 	}
 
 	if _, err := os.Open("non-existing"); err != nil {
